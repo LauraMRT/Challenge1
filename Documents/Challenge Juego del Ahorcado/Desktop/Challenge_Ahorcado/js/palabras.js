67337c2d97a1446a0d6f51ca5fb1c4d9d5b266bc
lauraMRT <lauramartinez.9918@gmail.com>
2022-09-29 13:54:27 -0500

    Challenge2

---
+++ Documents/Challenge Juego del Ahorcado/Desktop/Challenge_Ahorcado/js/palabras.js
@@ -0,0 +1,25 @@
+let input = document.getElementById("agregarPalabra");
+let palabrasLocal;
+const btnAgregar = document.getElementById("btnAgregar");
+
+const palabras = [
+  "MANZANA",
+  "CASA",
+  "CREMA",
+  "MOTEL",
+  "ESTADIO",
+  "PERSONA",
+  "EQUIPO",
+  "GATO",
+  "TIJERAS",
+  "HOTEL",
+  "ABEJA",
+  "PEZ",
+  "DELFIN",
+  "MOUSE",
+  "DINERO",
+  "BOLSO",
+  "LIBRO",
+  "CABALLO",
+  "CARGADOR",
+];
